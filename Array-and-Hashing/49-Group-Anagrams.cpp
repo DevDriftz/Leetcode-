5b6747b775b07a6014ed6df923f018453b1d9e84
DevDriftz <akshatsharmabhrigu@gmail.com>
2025-12-16 15:27:27 +0530

    N Queens solved

--- Array-and-Hashing/49-Group-Anagrams.cpp
+++ Array-and-Hashing/49-Group-Anagrams.cpp
@@ -5,37 +5,40 @@ using namespace std;
 
 class Solution{
 public:
-    void solve(vector<pair<string,string>>& v,vector<vector<string>>& ans,int i){
 
-        if(i >= v.size()) return;
+    void solve(vector<string>& strs, vector<vector<string>>& ans, int i){
+        // base case
+        if(i >= strs.size()) return;
 
         vector<string> baggu;
-        baggu.push_back(v[i].second); // original string
+        baggu.push_back(strs[i]);
 
-        while(i + 1 < v.size() && v[i].first == v[i + 1].first){
-            baggu.push_back(v[i + 1].second);
+        // group all equal adjacent strings
+        while(i + 1 < strs.size() && strs[i] == strs[i + 1]){
+            baggu.push_back(strs[i + 1]);
             i++;
         }
 
         ans.push_back(baggu);
-        solve(v, ans, i + 1);
+
+        // move to next new group
+        solve(strs, ans, i + 1);
     }
 
     vector<vector<string>> groupAnagrams(vector<string>& strs){
         vector<vector<string>> ans;
-        vector<pair<string,string>> v;
 
-        // store sorted + original
-        for(auto s : strs){
-            string sorted = s;
-            sort(sorted.begin(), sorted.end());
-            v.push_back({sorted, s});
+        // sort characters inside each string
+        for(int i = 0; i < strs.size(); i++){
+            sort(strs[i].begin(), strs[i].end());
         }
 
-        // sort by sorted string
-        sort(v.begin(), v.end());
+        // sort the whole array
+        sort(strs.begin(), strs.end());
 
-        solve(v, ans, 0);
+        // recursive grouping
+        solve(strs, ans, 0);
+
         return ans;
     }
 };
@@ -44,7 +47,7 @@ int main(){
     Solution s1;
     vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
 
-    auto res = s1.groupAnagrams(strs);
+    vector<vector<string>> res = s1.groupAnagrams(strs);
 
     for(auto &group : res){
         for(auto &word : group){
@@ -52,5 +55,6 @@ int main(){
         }
         cout << endl;
     }
+
     return 0;
 }
