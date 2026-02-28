85e3d845bb13b121d61432e47e5ecd88e001febb
DevDriftz <akshatsharmabhrigu@gmail.com>
2026-02-28 17:42:34 +0530

    chat gpt kra h nqueens2.cpp

---
+++ Backtracking/N-Queens2.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+class Solution {
+public:
+  void print(vector<vector<char>>& board){
+    for(auto &row : board){
+      for(auto &col : row){
+        cout << col << " ";
+      }
+      cout << endl;
+    }
+  }
+
+  bool issafe(vector<vector<char>>& board, int row, int col, int n) {
+
+    // check left side of row
+    for(int i = 0; i < col; i++)
+        if(board[row][i] == 'Q')
+            return false;
+
+    // check upper-left diagonal
+    for(int i=row, j=col; i>=0 && j>=0; i--, j--)
+        if(board[i][j] == 'Q')
+            return false;
+
+    // check lower-left diagonal
+    for(int i=row, j=col; i<n && j>=0; i++, j--)
+        if(board[i][j] == 'Q')
+            return false;
+
+    return true;
+  } 
+
+  void recurse(vector<vector<char>>& board, int col, int &ans){
+    int n = board.size();
+
+    if(col == n){
+      ans++;          // found one valid solution
+      return; 
+    }
+
+    for(int row = 0; row < n; row++){
+      if(issafe(board, row, col, n)){
+        board[row][col] = 'Q';
+        recurse(board, col+1, ans);
+        board[row][col] = '.';   // backtrack
+      }
+    }
+  }
+
+  int totalNQueens(int n){
+    vector<vector<char>> board(n, vector<char>(n,'.'));
+
+    int ans = 0;
+    recurse(board, 0, ans);
+
+    return ans;
+  }
+};
+
+int main() {
+  Solution s1; 
+  cout << "total soultions possible : " << s1.totalNQueens(4)<<endl; // 2 
+  return 0;
+}
